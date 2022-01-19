4a2db701fca82f5e958f83e813961821c15bcd27
aprilahijriyan <hijriyan23@gmail.com>
2022-01-19 21:50:27 +0700

    🎨 Improved formatting: run pre-commit

--- fastack_migrate/globals.py
+++ fastack_migrate/globals.py
@@ -3,10 +3,12 @@ from werkzeug.local import LocalProxy
 
 from fastack_migrate import MigrateConfig
 
+
 def _get_migrate_config():
     migrate = getattr(state, "migrate", None)
     if not isinstance(migrate, MigrateConfig):
         raise RuntimeError("fastack-migrate is not installed")
     return migrate
+
 
 migrate: MigrateConfig = LocalProxy(_get_migrate_config)
