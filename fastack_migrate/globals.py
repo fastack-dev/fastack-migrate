6d951532693a1d5601615a49f952e42209f7058d
aprilahijriyan <hijriyan23@gmail.com>
2022-02-09 14:58:20 +0700

    🐛 FIX: update fastack version to 4.5.0

--- fastack_migrate/globals.py
+++ fastack_migrate/globals.py
@@ -1,12 +1,14 @@
 from fastack.globals import state
-from werkzeug.local import LocalProxy
+from fastack.local import LocalProxy
 
 from fastack_migrate import MigrateConfig
 
-def _get_migrate_config():
+
+def _get_migrate_config() -> MigrateConfig:
     migrate = getattr(state, "migrate", None)
     if not isinstance(migrate, MigrateConfig):
         raise RuntimeError("fastack-migrate is not installed")
     return migrate
+
 
 migrate: MigrateConfig = LocalProxy(_get_migrate_config)
